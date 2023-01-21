34deed8e6642b7eb46bd415dfc4642d37330a321
SpecialistSteak <102715674+SpecialistSteak@users.noreply.github.com>
2023-01-21 21:52:48 +0530

    fixed uninstall.ps1 and added better comments for install.ps1

--- Install/Windows_JAR/install.ps1
+++ Install/Windows_JAR/install.ps1
@@ -8,18 +8,27 @@ New-Item -ItemType directory "C:\Users\$env:USERNAME\.Tasker\src\main\resources"
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
 
 # Download the TaskerCLI jar files
+Write-Output "Downloading TaskerCLI.jar..."
 Invoke-WebRequest https://github.com/SpecialistSteak/TaskerCLI/raw/master/TaskerCLI.jar -OutFile "C:\Users\$env:username\.Tasker\TaskerCLI.jar"
+
+Write-Output "Done. Downloading tasker.ps1..."
 Invoke-WebRequest https://raw.githubusercontent.com/SpecialistSteak/TaskerCLI/master/Install/Windows_JAR/tasker.ps1 -OutFile "C:\Users\$env:username\.Tasker\tasker.bat"
 
+Write-Output "Done. Downloading uninstall.ps1..."
 Invoke-WebRequest https://raw.githubusercontent.com/SpecialistSteak/TaskerCLI/master/Install/Windows_JAR/uninstall.ps1 -OutFile "C:\Users\$env:username\.Tasker\uninstall.bat"
 
+Write-Output "Done. Downloading javadoc.zip..."
 Invoke-WebRequest https://github.com/SpecialistSteak/TaskerCLIJavaDoc/archive/refs/heads/main.zip -OutFile "C:\Users\$env:username\.Tasker\javadoc.zip"
+
+Write-Output "Done. Extracting javadoc.zip..."
+
 Expand-Archive -Path "C:\Users\$env:USERNAME\.Tasker\javadoc.zip" -DestinationPath "C:\Users\$env:USERNAME\.Tasker"
 Rename-Item "C:\Users\$env:USERNAME\.Tasker\TaskerCLIJavaDoc-main" "C:\Users\$env:USERNAME\.Tasker\javadoc"
 Remove-Item "C:\Users\$env:USERNAME\.Tasker\javadoc.zip"
 
+Write-Output "Done. Adding .Tasker to PATH..."
 # Save tasker.ps1 to path
 $env:Path += ";C:\Users\$env:USERNAME\.Tasker"
 
 # Print a message to confirm the installation
-Write-Output "TaskerCLI has been installed to the C:\Users\$env:USERNAME\.Tasker directory."
+Write-Output "Done! TaskerCLI has been installed to the C:\Users\$env:USERNAME\.Tasker directory."
