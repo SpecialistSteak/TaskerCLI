b6d6920d1b3e3d3bb07e46126c74e43c3577bc56
SpecialistSteak <102715674+SpecialistSteak@users.noreply.github.com>
2023-01-21 18:55:31 +0530

    added javadoc

---
+++ javadoc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.specialistSteak"},{"l":"org.specialistSteak.dataType"},{"l":"org.specialistSteak.MS"},{"l":"org.specialistSteak.utils"}];updateSearchResults();
